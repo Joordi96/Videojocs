62f7f6cef7c0e9f77404f7476b2a4252da140ae1
Joordi96 <jordi.francesch96@gmail.com>
2026-08-02 17:43:21 -0400

    +

--- Projectes UNITY/Assets/02_UNIR_Motores_1_Laberinto/Scripts/DetectarSoldadoConRaycast.cs
+++ Projectes UNITY/Assets/02_UNIR_Motores_1_Laberinto/Scripts/DetectarSoldadoConRaycast.cs
@@ -1,16 +1,22 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class OnTriggerDetecarSoldadoConRaycast : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Camera vistaJugador;
+    [SerializeField] LayerMask layermask;
+    [SerializeField] float distanciaMaxima;
     void Update()
     {
-        
+        Ray rayo = vistaJugador.ViewportPointToRay(new Vector3 (0.5f, 0.5f, 0f));
+
+
+        if (Physics.Raycast(rayo, out RaycastHit hit, distanciaMaxima))
+        {
+            if (hit.collider.transform.root.CompareTag("TrampaSoldado"))
+            {
+                Debug.Log("Estas apuntando a un soldado!");
+            }
+        }
     }
 }
