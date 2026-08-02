846db4ce230b536d1344c5b849db0cfce6fa2f78
Joordi96 <jordi.francesch96@gmail.com>
2026-08-01 23:28:18 -0400

    Mejoras

--- Projectes UNITY/Assets/02_UNIR_Motores_1_Laberinto/Scripts/ControladorMensajes.cs
+++ Projectes UNITY/Assets/02_UNIR_Motores_1_Laberinto/Scripts/ControladorMensajes.cs
@@ -6,11 +6,20 @@ public class ControladorMensajes : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textoMensaje;
 
+    [SerializeField] private CanvasGroup canvasGroupInteraccion;
+    [SerializeField] private TextMeshProUGUI textoInteraccion;
+
     private Coroutine coroutineActual;
 
     private void Awake()
     {
         textoMensaje.gameObject.SetActive(false);
+        textoInteraccion.gameObject.SetActive(true);
+
+        canvasGroupInteraccion.alpha = 0f;
+
+        canvasGroupInteraccion.interactable = false;
+        canvasGroupInteraccion.blocksRaycasts = false;
     }
 
     public void MostrarMensaje(string mensaje, float duracion)
@@ -37,5 +46,16 @@ public class ControladorMensajes : MonoBehaviour
 
         textoMensaje.gameObject.SetActive(false);
         coroutineActual = null;
+    }
+
+    public void MostrarInteraccion(string mensaje)
+    {
+        textoInteraccion.text = mensaje;
+        canvasGroupInteraccion.alpha = 1f;
+    }
+
+    public void OcultarInteraccion()
+    {
+        canvasGroupInteraccion.alpha = 0f;
     }
 }
---
+++ Projectes UNITY/Assets/02_UNIR_Motores_1_Laberinto/Scripts/DetectarBotonConOverlap.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class DetectarBotonConOverlap : MonoBehaviour
+{
+    [SerializeField] float radioDeteccion = 3f;
+    [SerializeField] LayerMask layerMask;
+    private ControladorMensajes controladorMensajes;
+    private bool hayBoton;
+    private bool textoVisible = false;
+
+    public void Awake()
+    {
+        controladorMensajes =
+                    FindFirstObjectByType<ControladorMensajes>();
+
+        if (controladorMensajes == null)
+        {
+            Debug.LogError(
+                "No se ha encontrado un ControladorMensajes en la escena."
+            );
+        }
+    }
+    public void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radioDeteccion, layerMask);
+
+        hayBoton = false;
+
+        for (int i = 0 ; i < colliders.Length ; i++)
+        {
+            if (colliders[i].CompareTag("Boton"))
+            {
+                
+                hayBoton = true;
+                break;
+            }
+        }
+
+
+
+        if (hayBoton && !textoVisible)
+        {
+            controladorMensajes.MostrarInteraccion("Pulsa E para activar");
+            textoVisible = true;
+        }
+        else if (!hayBoton && textoVisible)
+        {
+            controladorMensajes.OcultarInteraccion();
+            textoVisible = false;
+        }
+    }
+}
+
+
+//         Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Mathf.Infinity, layermask);
